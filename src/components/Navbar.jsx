8f43702d763d8866a51cc976c7f9b22208451185
Predictfutur <vikaschelluru@gmail.com>
2023-09-11 17:02:01 +0530

    vikas_enter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-cover rounded-full' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-          Kavya Sai Isheka &nbsp;
-            <span className='sm:block hidden'> | Yakkala</span>
+           Vikas &nbsp;
+            <span className='sm:block hidden'> | Chelluru</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
